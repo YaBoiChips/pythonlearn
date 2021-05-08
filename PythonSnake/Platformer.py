7c6e76c9d4466a1d0c4e9f01361a6b0df68edfb1
YaBoiChips <63213388+YaBoiChips@users.noreply.github.com>
2021-05-07 21:41:12 -0600

    cool grid system

--- PythonSnake/Platformer.py
+++ PythonSnake/Platformer.py
@@ -4,12 +4,31 @@ import pygame
 
 pygame.init()
 
+tilesize = 64
+
+world_data = [
+    [1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+]
+
+world = []
+for row in range(len(world_data)):
+    for col in range(len(world_data[row])):
+        tile_id = world_data[row][col]
+        if tile_id == 0:
+            continue
+        if tile_id == 1:
+            tile = pygame.Rect(col * tilesize, row * tilesize, tilesize, tilesize)
+            world.append(tile)
+
+
 clocks = pygame.time.Clock()
-size = width, height = 1000, 1000
+size = width, height = 1600, 1000
 speed = 1
 black = 0, 0, 0
 pos = [0.0, 0.0]
-
 
 keydownUp = False
 keydownDown = False
@@ -36,20 +55,26 @@ while 1:
 
     if keydownDown:
         pos[1] = pos[1] + speed * delta
-    #if ballrect.left < 0:
-        #speed[0] = -speed[0]
-        #pos[0] = 0
-    #if ballrect.right > width:
-        #speed[0] = -speed[0]
-        #pos[0] = width - ballrect.width
-    #if ballrect.top < 0:
-        #speed[1] = -speed[1]
-        #pos[1] = 0
-    #if ballrect.bottom > height:
-        #speed[1] = -speed[1]
-        #pos[1] = height - ballrect.height
+    if keydownUp:
+        pos[1] = pos[1] - speed * delta
+    if keydownLeft:
+        pos[0] = pos[0] - speed * delta
+    if keydownRight:
+        pos[0] = pos[0] + speed * delta
+
+    if pos[0] < 0:
+        pos[0] = 0
+    if pos[0] + ballrect.width > width:
+        pos[0] = width - ballrect.width
+    if pos[1] < 0:
+        pos[1] = 0
+    if pos[1] + ballrect.height > height:
+        pos[1] = height - ballrect.height
+
     ballrect.update(pos, ballrect.size)
 
     screen.fill(black)
     screen.blit(ball, ballrect)
+    for tile in world:
+        pygame.draw.rect(screen, (255, 255, 255), tile)
     pygame.display.flip()
