d3a33ac66341760cecd4d71aa1bd35cb189939eb
James Woods <jimmyawoods@gmail.com>
2021-05-09 21:27:59 -0500

    basic phsycis.

--- PythonSnake/physics.py
+++ PythonSnake/physics.py
@@ -1,5 +1,44 @@
+from pygame.math import Vector2
+import map
+
 def has_collision(a, b):
     x_overlap = (a.left < b.right) and (a.right > b.left)
     y_overlap = (a.top < b.bottom) and (a.bottom > b.top)
     return x_overlap and y_overlap
 
+class PhysicsObject:
+    _parent = None
+    _epsilon = 0.01
+
+    mass = 1.0
+    force = Vector2(0, 0)
+    velocity = Vector2(0, 0)
+    position = Vector2(0, 0)
+    
+    bounce = 0.6
+
+    def __init__(self, parent):
+        self._parent = parent
+
+    def update(self, delta, world):
+        # acceleration = force / mass
+        self.velocity = self.velocity + self.force / self.mass
+        self.position = self.position + self.velocity * delta
+        self.force = Vector2(0, 0)
+
+        # check collisions
+        for tile in world:
+            if has_collision(self._parent.get_rect(), tile.get_rect()):
+                # TODO: emit events
+                if tile.get_id() == map.tile_type["ground"]:
+                    # rewind position to edge of object
+                    self.position.y = tile.get_rect().top - self._parent.get_rect().height
+                    if self.velocity.magnitude_squared() > self._epsilon:
+                        self.add_force(Vector2(0, -1) * self.bounce * self.mass * self.velocity.y)
+                    self.velocity.y = 0
+                elif tile.get_id() == map.tile_type["kill"]:
+                    # reset position
+                    print("player died")
+
+    def add_force(self, force):
+        self.force = self.force + force
--- PythonSnake/player.py
+++ PythonSnake/player.py
@@ -2,16 +2,20 @@ import pygame
 from pygame.math import Vector2
 
 import map
+from physics import PhysicsObject
 
+GRAVITY = 0.003
 
 class Player:
     _rect = None
-    _pos = Vector2()
-    speed = 0.5
     _image = pygame.image.load("64x64.png")
+    _physics_object = None
+    
+    speed = 0.5
 
     def __init__(self):
         self._rect = self._image.get_rect()
+        self._physics_object = PhysicsObject(self)
 
     def is_grounded(self, world):
         y_overlap = False
@@ -25,8 +29,15 @@ class Player:
         return self._rect
 
     def update(self, delta, world, player_input):
-        self._pos.x = self._pos.x + player_input.x * self.speed * delta
-        self._rect.update(self._pos, self._rect.size)
+        self._physics_object.velocity.x = player_input.x * self.speed * delta
+
+        # apply forces
+        self._physics_object.add_force(Vector2(0, GRAVITY))
+        # compute physics
+        self._physics_object.update(delta, world)
+
+        # update pygame objects
+        self._rect.update(self._physics_object.position, self._rect.size)
 
     def draw(self, screen):
         screen.blit(self._image, self._rect)
